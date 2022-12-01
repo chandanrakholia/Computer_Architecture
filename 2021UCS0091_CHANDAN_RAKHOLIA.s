f60c1fb78cf99efc791e2cf6727311b356933001
chandanrakholia <106094072+chandanrakholia@users.noreply.github.com>
2022-12-01 11:16:28 +0530

    Add files via upload

--- 2021UCS0091_CHANDAN_RAKHOLIA.s
+++ 2021UCS0091_CHANDAN_RAKHOLIA.s
@@ -1,14 +1,18 @@
 .section .data
     @num1: .word  0B01111111111010111000000000000000, 0B01111111111000100100000000000000                                     
-                @ 11.5 + 20.5 = 32
     @num1: .word 0x00220000, 0x00240000
     @num1: .word 0x001c0000, 0x00240000
-    num1: .word 0x0020000, 0x80010000
+    @num1: .word 0x00200000, 0x80010000
     @num1: .word 0x00240000, 0x001c0000
     @num1: .word 0xffe80000, 0x7fe00000
-    result: .skip 1
-    Addresult: .skip 8
-    MultiplicationResult: .skip 8
+    @num1 : .word 0x7fe00000, 0xffe80000
+    @num1 : .word 0x00260000, 0x00330000   @T1 0x0059A000 0x00368000
+    num1 : .word 0x00260000, 0x80330000   @T2 0x8059A000 0x802F0000
+    @num1 : .word 0x80260000, 0x00330000   @T3 0x8059A000 0x002F0000
+
+
+    Addresult: .byte 0,0,0,0
+    MultiplicationResult: .byte 0,0,0,0
 .section .text
 .global _start
     case_of_shiftby1:
@@ -20,7 +24,7 @@
         mov r9, #1      @ store number of shift
         b next
     nfpmul:
-        @stmfd sp!, {r0-r9,lr}
+        stmfd sp!, {r1-r9,lr}
         ldr r0, =num1
         ldr r1, [r0] @ r1 contains n1.
         add r0, r0, #4
@@ -56,7 +60,8 @@
         lsr r4, #20
         add r6, r3, r4
         add r6, r6, r9
-        lsl r6, r6, #19      
+        lsl r6, r6, #20
+        lsr r6, r6, #1      
         add r5, r5, r6       @ r5 contain final expo.
         @sign                    @ _expo+matissa
         ldr r0, =0x80000000
@@ -64,10 +69,32 @@
         and r4, r2, r0
         eor r6, r3, r4
         add r5, r5, r6
-        ldr r0, =Addresult
-        str r5, [r0]
-        b end
-        @ldmfd sp!, {r0-r9,pc}
+        ldr r0, =[MultiplicationResult]
+        add r0, r0, #4
+        ldr r9, =0x0000ff
+        and r8, r5, r9
+        strb r8, [r0]
+
+        ldr r9, =0x0000ff00
+        and r8, r5, r9
+        lsr r8, r8, #8
+        sub r0, r0, #1
+        strb r8, [r0]
+
+        ldr r9, =0x00ff0000
+        and r8, r5, r9
+        lsr r8, r8, #16
+        sub r0, r0, #1
+        strb r8, [r0]
+
+        ldr r9, =0xff000000
+        and r8, r5, r9
+        lsr r8, r8, #24
+        sub r0, r0, #1
+        strb r8, [r0]
+
+        b add
+        ldmfd sp!, {r1-r9,pc}
     swap:
         mov r0, r1
         mov r1, r2
@@ -100,7 +127,9 @@
         lsr r3, r3, #20
         lsr r4, r4, #20
         sub r0, r4, r3   @r0 contain difference of exponent
-        @mantisaa
+        
+        @mantisaa        @R4 contains bigger exponent
+        
         ldr r5, =0x0007ffff
         and r3, r1, r5     @r3 contains mantissa of number in r1
         and r6, r2, r5     @r6 contains mantissa of number in r2
@@ -112,6 +141,7 @@
         and r9, r2, r7
         lsr r8, r8, #31
         lsr r9, r9, #31
+        lsr r3, r3, r0
         mov r7, #-1
         cmp r8, #0
         bgt mul_r8
@@ -119,7 +149,6 @@
         cmp r9, #0
         bgt mul_r9
         next4:
-        lsr r3, r3, r0
         add r7, r3, r6
         ldr r9, =0x80000000
         and r1, r7, r9    @  R1 contain sign bit  
@@ -144,10 +173,35 @@
         add r7, r7, r4   @R7 contains mantissa + exponent
         @sign
         add r7, r7, r1
+        mov r5, r7
+        ldr r0, =[Addresult]
+        add r0, r0, #4
+        ldr r9, =0x0000ff
+        and r8, r5, r9
+        strb r8, [r0]
+
+        ldr r9, =0x0000ff00
+        and r8, r5, r9
+        lsr r8, r8, #8
+        sub r0, r0, #1
+        strb r8, [r0]
+
+        ldr r9, =0x00ff0000
+        and r8, r5, r9
+        lsr r8, r8, #16
+        sub r0, r0, #1
+        strb r8, [r0]
+
+        ldr r9, =0xff000000
+        and r8, r5, r9
+        lsr r8, r8, #24
+        sub r0, r0, #1
+        strb r8, [r0]
         b end;
 
 _start:
-    @bl nfpmul
+    bl nfpmul
+    add:
     bl nfpadd
     end:
         @ Given Task Done
